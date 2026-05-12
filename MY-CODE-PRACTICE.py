89c2762585aa454cc7d7b886ae4f83bf2139c203
hassanmehmuod <hassan.mehmuod@gmail.com>
2026-05-12 18:40:19 +0500

    Create MY-CODE-PRACTICE.py

---
+++ MY-CODE-PRACTICE.py
@@ -0,0 +1,9 @@
+studentname = "Ali"
+studentage = 35
+studentheight = 5.9
+
+print("Name: " + studentname)
+print("Age: " + str(studentage))
+print("Height: " + str(studentheight))
+
+print("Ends here")
