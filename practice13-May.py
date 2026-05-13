4b08a0f54294edae5b3c9fb648ef135eda9457e1
hassanmehmuod <hassan.mehmuod@gmail.com>
2026-05-13 18:24:36 +0500

    Create practice13-May.py

---
+++ practice13-May.py
@@ -0,0 +1,17 @@
+productid = 45
+price = 5000
+tax = 50
+total = price + tax
+print(productid)
+print(price)
+print(tax)
+print(total)
+
+discount = 5.5 
+netTotal = total -  discount
+print(discount)
+print(netTotal)
+
+productwarehouse = "Warehouse abc INC"
+productTitle= "IPhone 17 Pro Max"
+productsale = 50000
