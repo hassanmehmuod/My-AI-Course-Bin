fe9d9402af15a4ed0646497f3096ea0c885bac6e
hassanmehmuod <hassan.mehmuod@gmail.com>
2026-05-25 18:42:23 +0500

    Create MyPractice.py

---
+++ MyPractice.py
@@ -0,0 +1,38 @@
+# print("Welcome to my assignment")
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# height= float(input("Enter your height in centimenters: "))
+
+# print("Your written name is: ",name)
+# print(type(name))
+# print("Your age is: ",age)
+# print(type(age))
+# print("Your height is: ",height)
+# print(type(height))
+
+# newage = age + 5
+# print(newage)
+# newage = age - 5
+# print(newage)
+# newage = age * 5
+# print(newage)
+# newage = age / 5
+# print(newage)
+
+
+# print(type(name))
+# print(len(name))
+
+# # for i in name:
+# #     print(i)
+
+age = int(input("Enter ur age: "))
+if age==0:
+    print("age is zero")
+elif age>20:
+    print("he is adult")
+elif age<20:
+    print("he is kid")
+
+else:
+    print("age is invalid")
