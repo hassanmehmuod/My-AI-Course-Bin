c2bc28c72ae58f59d16ef34a9601e6fa3de8118b
hassanmehmuod <hassan.mehmuod@gmail.com>
2026-06-02 18:50:21 +0500

    numpy practice

---
+++ usa-realestate-practice.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+brokeredby , status , price , bed , bath= np.genfromtxt('RealEstate-USA.csv', delimiter= ',', usecols = (0,1,2,3,4), unpack=True, dtype=None,skip_header=1)
+print(brokeredby)
+print(status)
+print(price)
+print(bed)
+print(bath)
+print("Real estate mean;  ", np.mean(price))
+print("Real estate average;  ", np.average(price))
+print("Real estate percentile;  ", np.percentile(price, 25))
+print("Real estate percentile:  ",np.percentile(price, 75))
+print("Real estate min:  ",np.min(price))
+print("Real estate max:  ",np.max(price))
