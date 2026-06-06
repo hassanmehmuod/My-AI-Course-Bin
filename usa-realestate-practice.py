ebc6b6ccdebe2bf0e447ec389e7622e5e32d9e7f
hassanmehmuod <hassan.mehmuod@gmail.com>
2026-06-06 18:33:00 +0500

    Update usa-realestate-practice.py

--- usa-realestate-practice.py
+++ usa-realestate-practice.py
@@ -7,9 +7,16 @@ print(status)
 print(price)
 print(bed)
 print(bath)
+
+
 print("Real estate mean;  ", np.mean(price))
 print("Real estate average;  ", np.average(price))
 print("Real estate percentile;  ", np.percentile(price, 25))
 print("Real estate percentile:  ",np.percentile(price, 75))
 print("Real estate min:  ",np.min(price))
 print("Real estate max:  ",np.max(price))
+
+print("Real estate Price square: " , np.square(price))
+print("Real estate Price sqrt: " , np.sqrt(price))
+print("Real estate Price pow: " , np.power(price,price))
+print("Real estate Price abs: " , np.abs(price))
